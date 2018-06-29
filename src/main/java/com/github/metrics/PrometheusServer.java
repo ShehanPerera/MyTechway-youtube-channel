0efb10b9f7435469ca83a2b61b5f0573a02cc1ad
shehanPerera <12shehanakalanka@gmail.com>
2018-06-29 11:29:21 +0530

    Update structure

--- src/main/java/com/github/metrics/PrometheusServer.java
+++ src/main/java/com/github/metrics/PrometheusServer.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.Metrics;
+package com.github.metrics;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
