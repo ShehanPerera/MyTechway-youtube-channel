606523454a7f363f4c99c4018c15ffcb601744df
shehanPerera <12shehanakalanka@gmail.com>
2018-06-29 11:29:43 +0530

    Update structure

--- src/main/java/com/github/sorting/RadixSort.java
+++ src/main/java/com/github/sorting/RadixSort.java
@@ -12,10 +12,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.Sorting;
+package com.github.sorting;
 
 import com.codahale.metrics.Timer;
-import com.github.Metrics.MetricsServer;
+import com.github.metrics.MetricsServer;
 
 import java.util.Arrays;
 
