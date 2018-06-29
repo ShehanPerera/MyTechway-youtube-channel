3dffb435b4682e76ddbc9dce569bcabd5572187d
shehanPerera <12shehanakalanka@gmail.com>
2018-06-29 11:37:20 +0530

    Add java docs

--- src/main/java/com/github/sorting/SelectionSort.java
+++ src/main/java/com/github/sorting/SelectionSort.java
@@ -17,6 +17,9 @@ package com.github.sorting;
 import com.codahale.metrics.Timer;
 import com.github.metrics.MetricsServer;
 
+/**
+ * This class used for sort an array using Selection sort method
+ */
 public class SelectionSort {
 
     private Timer.Context context;
