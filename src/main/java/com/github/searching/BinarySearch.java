ed807e02ae2e475490037b2232650992ea216380
shehanPerera <12shehanakalanka@gmail.com>
2018-07-02 09:04:14 +0530

    Code rearrange

--- src/main/java/com/github/searching/BinarySearch.java
+++ src/main/java/com/github/searching/BinarySearch.java
@@ -2,23 +2,23 @@ package com.github.searching;
 
 public class BinarySearch {
 
-    public boolean found = false;
+    private boolean found = false;
 
     /**
      * A recursive binary search function. It returns found massage
-     of x in given array arr[l..r] is present,
-     otherwise returns not found massage
-     * @param list data set for search
-     * @param element  is search element
+     * of x in given array arr[l..r] is present,
+     * otherwise returns not found massage
+     *
+     * @param list    data set for search
+     * @param element is search element
      * @return found or not found massage
      */
     public String BinarySearch(int list[], int element) {
 
         String result;
-
         int n = list.length;
-        boolean found2 = BinarySearching(list, 0, n - 1, element);
-        if (found2) {
+        boolean found = BinarySearching(list, 0, n - 1, element);
+        if (found) {
             result = "Found";
         } else {
             result = "Not Found";
@@ -28,25 +28,25 @@ public class BinarySearch {
     }
 
     /**
-     *Returns index of x if it is present in arr[l..r] true , else return false
-     * @param list data set for search
-     * @param l location for first element in array
-     * @param n length for array
+     * Returns index of x if it is present in arr[l..r] true , else return false
+     *
+     * @param list    data set for search
+     * @param l       location for first element in array
+     * @param n       length for array
      * @param element is search element
      * @return true or false massage
      */
-    public boolean BinarySearching(int list[], int l, int n, int element) {
+    private boolean BinarySearching(int list[], int l, int n, int element) {
 
-        int x = element;
         if (n >= l) {
             int mid = l + (n - l) / 2;
 
             if (list[mid] == element) {
                 found = true;
             } else if (list[mid] > element) {
-                return BinarySearching(list, l, mid - 1, x);
+                return BinarySearching(list, l, mid - 1, element);
             } else if (list[mid] < element) {
-                return BinarySearching(list, mid + 1, n, x);
+                return BinarySearching(list, mid + 1, n, element);
             }
             return found;
         }
